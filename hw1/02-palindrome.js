c3bc76fe20aab03c4cfbadda0e4008132c1e79df
Norcrossover <59990490+Norcrossover@users.noreply.github.com>
2024-04-14 17:46:10 -0700

    Finished creating the palindrome exercise

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,35 @@
 const elem = document.querySelector('input');
+const resultDiv = document.querySelector('.result');
 
 elem.addEventListener('input', handleInput);
+
+function isPalindrome(userInput) {
+  if (typeof(userInput) != "number")
+    return false;
+
+  if (userInput < 0)
+    return false;
+  
+  let orignalNumber = userInput;
+  let reversedNumber = 0;
+
+  while (userInput > 0) {
+    reversedNumber = reversedNumber * 10 + userInput % 10;
+    userInput = Math.floor(userInput / 10);
+  }
+
+  return orignalNumber === reversedNumber;
+}
+
+function handleInput(event) {
+  const userInput = Number(event.target.value);
+  let msg = "";
+  if (isPalindrome(userInput)) {
+    msg = "Yes. This is a palindrome!";
+    resultDiv.style.color = 'green';
+  } else {
+    msg = "No. Try again.";
+    resultDiv.style.color = 'red';
+  }
+  resultDiv.textContent = msg;
+}
