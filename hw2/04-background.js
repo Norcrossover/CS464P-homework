dc3a6fbd2c5d2e7f42063c3b5ec884f7df1f0b20
Norcrossover <59990490+Norcrossover@users.noreply.github.com>
2024-04-19 18:26:38 -0700

    Started exercsie 4

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,12 @@
-// Add your code here
+// define page elements
+const intervalButton = document.getElementById("intervalButton");
+const pageBackground = document.getElementById("pageBackground");
+
+// event listeners
+intervalButton.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  // get the interval length
+  // change the interval button
+  // change to a random color when interval timing changes
+}
