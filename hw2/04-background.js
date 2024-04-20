74564067948583b28155669f8a7a149000d5a3e2
KobeNorcross <kobenorcross@gmail.com>
2024-04-19 22:09:15 -0700

    Some progress on background

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,12 +1,18 @@
 // define page elements
 const intervalButton = document.getElementById("intervalButton");
+const numberInput = document.getElementById("numberInput");
 const pageBackground = document.getElementById("pageBackground");
 
 // event listeners
 intervalButton.addEventListener("click", handleClick);
 
 function handleClick(event) {
-  // get the interval length
+  const intervalLength = numberInput.value;
   // change the interval button
+  if (intervalButton.value === "Start") {
+    intervalButton.value = "Stop";
+  } else {
+    intervalButton.value = "Start";
+  }
   // change to a random color when interval timing changes
 }
