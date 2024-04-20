1ad4591d1072a5b001d5c418d14925a0078b3161
Norcrossover <59990490+Norcrossover@users.noreply.github.com>
2024-04-19 18:08:53 -0700

    Exercise 03

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,26 @@
 // Add your code here
+const input = document.getElementById("userInput");
 
-input.addEventListener('keydown', handleKeyDown);
+function handleKeyDown(event) {
+  const textDiv = document.getElementById("paragraph");
+  const textList = textDiv.textContent.split(" ");
+  const userQuery = input.value.toLowerCase();
+
+  if (userQuery === "") {
+    textDiv.innerHTML = textDiv.textContent;
+    return;
+  }
+
+  const highlightedTextList = textList.map((text) => {
+    const lowerCaseText = text.toLowerCase();
+
+    if (lowerCaseText.includes(userQuery)) {
+      return text.split(userQuery).join(`<mark>${userQuery}</mark>`);
+    }
+    return text;
+  });
+
+  textDiv.innerHTML = highlightedTextList.join(" ");
+}
+
+input.addEventListener("keydown", handleKeyDown);
